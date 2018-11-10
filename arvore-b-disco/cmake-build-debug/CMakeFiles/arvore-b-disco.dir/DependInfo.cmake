0e0810a72811b6699120971c74e5cb5caeb5e158
Mariana Ferreira <ferreira_mariana@id.uff.br>
2018-11-10 16:00:27 -0200

    codigo base da vanessa

---
+++ arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/arvore_b.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/arvore_b.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cliente.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/cliente.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/lista_clientes.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/lista_clientes.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/lista_nos.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/lista_nos.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/metadados.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/metadados.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/no.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/no.c.o"
+  "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/test_arvore_b.c" "/Users/vanessa/Dropbox/Documents/Disciplinas/ED/Aulas/11-ArvoreB/arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/test_arvore_b.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ arvore-b-disco/cmake-build-debug/CMakeFiles/arvore-b-disco.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arvore-b-disco.dir/arvore_b.c.o"
+  "CMakeFiles/arvore-b-disco.dir/cliente.c.o"
+  "CMakeFiles/arvore-b-disco.dir/lista_clientes.c.o"
+  "CMakeFiles/arvore-b-disco.dir/lista_nos.c.o"
+  "CMakeFiles/arvore-b-disco.dir/metadados.c.o"
+  "CMakeFiles/arvore-b-disco.dir/no.c.o"
+  "CMakeFiles/arvore-b-disco.dir/test_arvore_b.c.o"
+  "arvore-b-disco.pdb"
+  "arvore-b-disco"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arvore-b-disco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
